835e91c74e4aebeb05fa6c9be293720d43cc5999
Houss932 <lamech_h@etna-alternance.net>
2017-01-31 21:19:55 +0100

    html
    partie html

--- Views/accueil/accueil.php
+++ Views/accueil/accueil.php
@@ -1,1 +1,45 @@
-Accueil
+<div class="containerPage">
+    <div class="encart" align="center">
+        <div class="white" style="width: 70%; margin-top: 4%; border-radius: 50px;">
+            <br>
+            <div class="input-field" style="width: 60%; margin-top: 2%">
+                <input id="first_name2" type="text" class="validate">
+                <label class="active" for="first_name2">Rechercher une recette</label>
+            </div>
+            <a class="btn-floating btn-large waves-effect waves-light orange darken-3"><i
+                    class="material-icons">search</i></a><br><br>
+        </div>
+    </div>
+    <div class="rowpage">
+        <div class="col s12 m7">
+            <div class="card horizontal">
+                <div class="card-image">
+                    <img src="http://www.photos-gratuites.org/wp-content/gallery/photo-plat/photo-plat6.jpg">
+                </div>
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Acceder a la recette</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m7">
+            <div class="card horizontal">
+                <div class="card-image">
+                    <img src="http://pendacuisine.com/images/plats/Autres/steak%20de%20viande%20-%20autres.jpg">
+                </div>
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Acceder a la recette</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- Views/recette/recette.php
+++ Views/recette/recette.php
@@ -1,1 +1,91 @@
-Recette
+<div class="containerPage">
+    <div class="encart" align="center">
+        <div class="white" style="width: 70%; margin-top: 4%; border-radius: 50px;">
+            <br>
+            <h3>Ajout d'une recette</h3>
+            <br>
+        </div>
+    </div>
+    <div class="rowpage">
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <h5>Utilisateur</h5>
+                    <div class="col s12">
+                        <div class="input-field inline" style="width: 40%">
+                            <input id="pseudo" type="text" class="validate">
+                            <label for="pseudo" data-error="wrong" data-success="right">pseudo</label>
+                        </div>
+                        <div class="input-field inline" style="width: 58%">
+                            <input id="email" type="email" class="validate">
+                            <label for="email" data-error="wrong" data-success="right">Email</label>
+                        </div>
+                    </div>
+                </div>
+                <h5>Votre Recette</h5>
+                <div class="row">
+                    <div class="input-field col s8">
+                        <input id="name" type="text" class="validate">
+                        <label for="name">Nom de la recette</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <input disabled id="Temps" type="text" class="validate">
+                        <label for="Temps">Temps de préparation</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <select disabled>
+                            <option value="" disabled selected>Choisir une option</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                            <option value="3">Option 3</option>
+                        </select>
+                        <label>Type de plat</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <select disabled>
+                            <option value="" disabled selected>Choisir une option</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                            <option value="3">Option 3</option>
+                        </select>
+                        <label>Difficulté</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <select disabled>
+                            <option value="" disabled selected>Choisir une option</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                            <option value="3">Option 3</option>
+                        </select>
+                        <label>Cout</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input id="quantite" type="text" class="validate">
+                        <label for="quantite">Quantité</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input id="ingredient" type="text" class="validate">
+                        <label for="ingredient">Ingredient</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input id="quantite" type="text" class="validate">
+                        <label for="quantite">Quantité</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input id="ingredient" type="text" class="validate">
+                        <label for="ingredient">Ingredient</label>
+                    </div>
+                    <a class="btn-floating btn-large waves-effect waves-light right red"><i class="material-icons">add</i></a>
+                </div>
+                <div align="center">
+                    <button class="btn waves-effect waves-light orange darken-3" type="submit" name="action">Envoyer la
+                        recette
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
