f719cefa223b34aade393ea18012b6832d8fb945
Houss932 <lamech_h@etna-alternance.net>
2017-02-02 20:01:49 +0100

    tab ingredient + tab quantité

--- models/RecetteModel.php
+++ models/RecetteModel.php
@@ -11,6 +11,17 @@ class RecetteModel extends Models
     public function addRecette($data) 
     {
         var_dump($data);
+        $ingredient = [];
+        $quantite = [];
+        echo $_POST["quantite". 0];
+        for ($i=0; isset($_POST["ingredient". $i]); $i++) {
+        	array_push($ingredient, $_POST["ingredient". $i]);
+        }
+        for ($j=0; isset($_POST["quantite". $j]); $j++) {
+            array_push($quantite, $_POST["quantite". $j]);
+        }
+        print_r($ingredient);
+        print_r($quantite);
         // $db = Models::Connection();
         // var_dump($_POST);
         // $db->
